d70f056556b28acd0f5b6419f513be5ce287466b
Arif5577 <arifalamm33@gmail.com>
2022-10-26 15:25:36 +0600

    404 not found page added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,12 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
+import NotFound from "../pages/NotFound";
+import Register from "../pages/Register";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+      { path: "*", element: <NotFound /> },
+    ],
   },
 ]);
 
---
+++ src/pages/NotFound.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <section className="py-24 bg-light flex text-center items-center justify-center">
+      <div className="container">
+        <h1 className=" lg:text-8xl text-4xl font-bold text-gray-600 tracking-wider">
+          404
+        </h1>
+        <h2 className="text-center font-bold text-xl uppercase text-gray-600">
+          No content found
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default NotFound;
