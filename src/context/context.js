cc34d02a630fdaf9edb371cc3d79c2366ff5bb7c
Arif5577 <arifalamm33@gmail.com>
2022-10-26 21:17:26 +0600

    authentication added

--- src/context/context.js
+++ src/context/context.js
@@ -1,12 +1,65 @@
-import { createContext } from "react";
-import { createUser, updateUser, LogOut } from "../utils/authServices.js";
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import auth from "../firebase/firebase.config.js";
 export const DataContext = createContext();
 
 const DataProvider = ({ children }) => {
-  const auth = { createUser, updateUser, LogOut };
+  const [user, setUser] = useState(null);
+  const [loader, setLoader] = useState(true);
+
+  // AUTH CONTROLLERS
+
+  //------Create user with email and password
+  const createUser = async (email, password) => {
+    setLoader(true);
+    return await createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //------Update User FullName and Photo
+  const updateUser = async (displayName, photoURL) => {
+    return await updateProfile(auth.currentUser, { displayName, photoURL });
+  };
+
+  //------SignIn With Google Account
+  const googleProvider = new GoogleAuthProvider();
+  const googleSignIn = async () => {
+    setLoader(true);
+    return await signInWithPopup(auth, googleProvider);
+  };
+
+  //------ User LogOut
+  const LogOut = async () => {
+    return await signOut(auth);
+  };
+
+  // COURSE CONTROLLERS
+
+  const authentication = { createUser, updateUser, LogOut, googleSignIn };
   const courses = {};
+
+  useEffect(() => {
+    const subscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        setLoader(false);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => subscribe();
+  }, []);
   return (
-    <DataContext.Provider value={{ ...auth, ...courses }}>
+    <DataContext.Provider
+      value={{ ...authentication, ...courses, user, loader }}
+    >
       {children}
     </DataContext.Provider>
   );
--- src/utils/authServices.js
+++
@@ -1,6 +0,0 @@
-// CREATE NEW USER
-export const createUser = async () => {};
-// UPDATE USER PROFILE
-export const updateUser = async () => {};
-//LOGOUT USER
-export const LogOut = async () => {};
