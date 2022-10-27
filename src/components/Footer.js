9da089c025efbdc79b57c46d12d3c640287f5da7
Arif5577 <arifalamm33@gmail.com>
2022-10-27 09:06:00 +0600

    responsive footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,69 +1,103 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="text-gray-600 bg-primary py-10 body-font">
+    <footer className="text-gray-600 bg-primary py-10 body-font border-t">
       <div className="container">
         <div className="flex flex-wrap md:text-left text-center order-first">
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
+            <h2 className=" font-semibold text-gray-900 tracking-widest text-sm mb-3">
+              QUICK LINKS
             </h2>
             <nav className="list-none mb-10">
               <li>
-                <a className="text-gray-600 hover:text-gray-800">First Link</a>
+                <Link to="/" className="text-gray-600 hover:text-gray-800">
+                  Home
+                </Link>
               </li>
               <li>
-                <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+                <Link
+                  to="/courses"
+                  className="text-gray-600 hover:text-gray-800"
+                >
+                  Courses
+                </Link>
               </li>
               <li>
-                <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+                <Link to="/blog" className="text-gray-600 hover:text-gray-800">
+                  Blog
+                </Link>
               </li>
               <li>
-                <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                <Link to="/faq" className="text-gray-600 hover:text-gray-800">
+                  Faq
+                </Link>
               </li>
             </nav>
           </div>
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+            <h2 className=" font-bold text-gray-900 tracking-widest text-sm mb-3">
               CATEGORIES
             </h2>
             <nav className="list-none mb-10">
               <li>
-                <a className="text-gray-600 hover:text-gray-800">First Link</a>
+                <Link to="/" className="text-gray-600 hover:text-gray-800">
+                  Home
+                </Link>
               </li>
               <li>
-                <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+                <Link
+                  to="/courses"
+                  className="text-gray-600 hover:text-gray-800"
+                >
+                  Courses
+                </Link>
               </li>
               <li>
-                <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+                <Link to="/blog" className="text-gray-600 hover:text-gray-800">
+                  Blog
+                </Link>
               </li>
               <li>
-                <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                <Link to="/faq" className="text-gray-600 hover:text-gray-800">
+                  Faq
+                </Link>
               </li>
             </nav>
           </div>
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
+            <h2 className=" font-bold text-gray-900 tracking-widest text-sm mb-3 uppercase">
+              Our Service
             </h2>
             <nav className="list-none mb-10">
               <li>
-                <a className="text-gray-600 hover:text-gray-800">First Link</a>
+                <Link to="/" className="text-gray-600 hover:text-gray-800">
+                  Home
+                </Link>
               </li>
               <li>
-                <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+                <Link
+                  to="/courses"
+                  className="text-gray-600 hover:text-gray-800"
+                >
+                  Courses
+                </Link>
               </li>
               <li>
-                <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+                <Link to="/blog" className="text-gray-600 hover:text-gray-800">
+                  Blog
+                </Link>
               </li>
               <li>
-                <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                <Link to="/faq" className="text-gray-600 hover:text-gray-800">
+                  Faq
+                </Link>
               </li>
             </nav>
           </div>
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+            <h2 className=" font-bold text-gray-900 tracking-widest text-sm mb-3">
               SUBSCRIBE
             </h2>
             <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
@@ -72,7 +106,7 @@ const Footer = () => {
                   htmlFor="footer-field"
                   className="leading-7 text-sm text-gray-600"
                 >
-                  Placeholder
+                  our newsletter
                 </label>
                 <input
                   type="text"
@@ -81,8 +115,8 @@ const Footer = () => {
                   className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 />
               </div>
-              <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                Button
+              <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-secondary border-0 py-2.5 px-6 focus:outline-none hover:bg-red-500 rounded text-sm">
+                Subscribe
               </button>
             </div>
             <p className="text-gray-500 text-sm mt-2 md:text-left text-center">
